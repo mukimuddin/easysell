96aa371c26cc8f8e2d298c2624f0aa0d26177e76
Md Mukim Uddin <mdmukimuddin.bd@gmail.com>
2026-05-06 15:17:29 +0600

    Fix port for cPanel deployment

--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import { Server } from 'socket.io';
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 const allowedSocketOrigin = process.env.SOCKET_ALLOWED_ORIGIN || (dev ? 'http://localhost:3000' : null);
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
